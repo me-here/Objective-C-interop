5526ba4a6316542fbff18b1157451321553a0a90
Mihir Thanekar <mihirthanekar@gmail.com>
2017-08-07 17:00:44 -0700

    Almost done with remaining problems

--- Problem Set/GuitarString/GuitarString/main.m
+++ Problem Set/GuitarString/GuitarString/main.m
@@ -13,9 +13,17 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
         // a. Create an instance of the class GuitarString
+        GuitarString *gS = [[GuitarString alloc] init];
+        
         // b. Create an NSError
+        NSError *error;
+        
         // c. Call the method pluck(velocity: Float)
+        [gS pluck: 0.7];
+        
         // d. Check if an error was returned
+        
+        
         // e. Log an error if one was returned
 
     }
